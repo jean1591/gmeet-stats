2811ab5f786a66839b8f435dfefbf7eb59878749
Jean Robertou <j-rob@live.fr>
2025-10-06 21:14:52 +0200

    Add looger to backend

--- gmeet-backend/src/app.module.ts
+++ gmeet-backend/src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SessionsModule } from './sessions/sessions.module';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [PrismaModule, SessionsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- gmeet-backend/src/main.ts
+++ gmeet-backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
   // Enable CORS for browser extension and future frontend
   app.enableCors({
